169f913d7c10afe0a5ea8d12d5d9070fe200b4ca
pedrosapiens <pedro@sapienslearning.com>
2021-11-07 11:00:03 +0100

    initial

---
+++ main.js
@@ -0,0 +1,13 @@
+// Tipos para datos primitivos
+// Sintaxis
+// let identificador: <tipo>
+// tipo string
+var mensaje;
+mensaje = 'Hola Mundo!';
+// mensaje = 12;
+// tipo number
+var resultado;
+resultado = 9.5;
+// tipo boolean
+var mayorEdad;
+mayorEdad = false;
---
+++ main.ts
@@ -0,0 +1,24 @@
+// Tipos para datos primitivos
+
+// Sintaxis
+
+// let identificador: <tipo>
+
+// tipo string
+
+let mensaje: string;
+
+mensaje = 'Hola Mundo!';
+
+// mensaje = 12;
+
+// tipo number
+
+let resultado: number;
+resultado = 9.5;
+
+// tipo boolean
+
+let mayorEdad: boolean;
+mayorEdad = false;
+
